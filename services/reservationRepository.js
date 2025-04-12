39e7b6246ae361866669d89872cd0467fbafb58a
Ambivalentt <stackov3r@gmail.com>
2025-04-11 19:07:36 -0500

    Refactorizacion al metodo create

--- services/reservationRepository.js
+++ services/reservationRepository.js
@@ -2,27 +2,53 @@ const reservation = require('../src/server.js')
 
 class ReservationRepository {
     static async create({ details, user_id, data_reservation }) {
-        Validation.details(details)
-        const date = new Date(data_reservation);
-        const startHour = new Date(date)
-        startHour.setMinutes(0,0,0)
+        Validation.details(details) //validacion details 
 
-        const endHour = new Date(date);
-        endHour.setMinutes(59,59,999)
+        const user = await this.getUserById(user_id)
+        if (!user) throw new Error(`Uset with id ${user_id} not found`)
 
-        const query = "SELECT * FROM users WHERE id = ?";
-        const [results] = await reservation.promise().query(query, [user_id]);
-        if (!results || results.length === 0) throw new Error('User not found user:');
-        const queryDate = "SELECT COUNT(*) AS total_reservations FROM reservation WHERE date_reservation >= ? AND date_reservation <= ?";
-        const [resultDate] = await reservation.promise().query(queryDate, [startHour, endHour]);
-        if (resultDate[0].total_reservations > 5) throw new Error('All dates already reserved');
-        const queryInsert = "INSERT INTO reservation (details, user_id, date_reservation) VALUES (?, ?, ?)";
-        const [result] = await reservation.promise().query(queryInsert, [details, user_id, data_reservation]);
-        const queryGet = "SELECT * FROM reservation WHERE id = ?";
-        const [newReservation] = await reservation.promise().query(queryGet, [result.insertId]);
-        return newReservation[0]
+        const [startHour, endHour] = this.getHourRange(data_reservation) //traemos la hora de inicio y final
+
+        const reservationCount = await this.countReservationsInHour(startHour, endHour) //agregamos la consulta de la hora
+        if(reservationCount >= 5) throw new Error('No more than 5 reservations per hour') //validamos si no hay mas de 5 resevaciones por hora
+
+        const query = "INSERT INTO reservation (details, user_id, date_reservation) VALUES (?, ?, ?)"
+        const [result] = await reservation.promise().query(query,[details, user_id, data_reservation]);
+        
+        return await this.getReservationById(result.insertId)
     }
 
+    static getHourRange(datetime) {
+        const date = new Date(datetime);
+        const start = new Date(date)
+        start.setMinutes(0, 0, 0)
+
+        const end = new Date(date);
+        end.setMinutes(59, 59, 999)
+        return [start, end]
+    }
+
+    static async getUserById(id) {
+        const userQuery = "SELECT * FROM users WHERE id = ?";
+        const [user] = await reservation.promise().query(userQuery, [id]);
+        return user || null
+    }
+
+    static async countReservationsInHour(start, end) {
+        const countQuery = `
+          SELECT COUNT(*) AS total
+          FROM reservation
+          WHERE date_reservation >= ? AND date_reservation <= ?
+        `;
+        const [results] = await reservation.promise().query(countQuery, [start, end]);
+        return results[0].total;
+    }
+
+    static async getReservationById(reservationId) {
+        const selectQuery = "SELECT * FROM reservation WHERE id = ?";
+        const [reservations] = await reservation.promise().query(selectQuery, [reservationId]);
+        return reservations[0] || null;
+    }
 }
 
 class Validation {
